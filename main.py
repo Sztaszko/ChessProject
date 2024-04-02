e2731328dae4c16d2001d08cf52fa8aa1af0d209
Sztaszko <majneimejl@gmail.com>
2024-04-02 20:37:38 +0200

    Initial architecture.

--- main.py
+++ main.py
@@ -2,13 +2,39 @@
 
 
 class Board():
-     def __init__(self):
-         pass
-         
+    """The Board class represents chessboard. It is responsible for movement validation and pieces arrangement.
+    """
+
+    def __init__(self) -> None:
+         self.square
+    
+    def move_piece(self, start_pos, end_pos):
+        pass
+
 
 class Piece():
-    def __init__(self):
+    def __init__(self) -> None:
         pass
     
     def move():
         pass
+
+class Player():
+    VALID_COLORS = ["white", "black"]
+
+    def __init__(self, color) -> None:
+
+        if color not in self.VALID_COLORS:
+            raise ValueError("Invalid color")
+        self.color = color
+
+    def make_move(self, piece: Piece, end_pos):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__== "__main__":
+    main()
