4d7ea61b4b58f6ababe8d50e6cf7814df5ef122f
Sztaszko <majneimejl@gmail.com>
2024-04-05 19:49:08 +0200

    Add movement for black pawn.

--- chess/pieces.py
+++ chess/pieces.py
@@ -21,12 +21,18 @@ class Pawn(Piece):
         if super()._is_in_board(end_pos):
             #TODO
             #add the en passant and the first move
-            #and what with black moves??
-            if end_pos[0] == start_pos[0] and end_pos[1] == start_pos[1]+1: #normal movement
-                return True
-            elif end_pos[1] == start_pos[1]+1 and (end_pos[0] == start_pos[0]+1 or end_pos[0] == start_pos[0]-1): #taking a piece
-                return True
-            else: return False
+            if self.color == 'white':
+                if end_pos[0] == start_pos[0] and end_pos[1] == start_pos[1]+1: #normal movement
+                    return True
+                elif end_pos[1] == start_pos[1]+1 and (end_pos[0] == start_pos[0]+1 or end_pos[0] == start_pos[0]-1): #taking a piece
+                    return True
+                else: return False
+            elif self.color == 'black':
+                if end_pos[0] == start_pos[0] and end_pos[1] == start_pos[1]-1: #normal movement
+                    return True
+                elif end_pos[1] == start_pos[1]-1 and (end_pos[0] == start_pos[0]-1 or end_pos[0] == start_pos[0]+1): #taking a piece
+                    return True
+                else: return False
         return False
     
 
